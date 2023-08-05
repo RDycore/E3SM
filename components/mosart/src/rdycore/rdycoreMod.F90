a26745e6533bf92514d5921d1a465fe969daf415
Gautam Bisht <gautam.bisht@pnnl.gov>
2023-08-04 20:07:00 -0700

    Adds a spatially homogeneous water source to RDycore
    Reads in a binary Petsc Vec (rain.bin) that is present in
    the rain directory. The Vec is has data in the following format:
    
    time_1 rain_1
    time_2 rain_2
    ...

--- components/mosart/src/rdycore/rdycoreMod.F90
+++ components/mosart/src/rdycore/rdycoreMod.F90
@@ -4,14 +4,21 @@ module rdycoreMod
 
   use petsc
   use rdycore
-  use RtmSpmd, only : mpicom_rof
-  use shr_kind_mod, only : r8 => shr_kind_r8
+  use RtmSpmd      , only : mpicom_rof, masterproc
+  use RtmVar       , only : iulog
+  use shr_kind_mod , only : r8 => shr_kind_r8
+  use shr_sys_mod  , only : shr_sys_flush
 
   implicit none
 
   private
 
   type(RDy) :: rdy_
+  Vec       :: rain_timeseries
+  PetscInt  :: rain_stride
+  PetscInt  :: rain_nstep
+  PetscInt  :: num_cells_owned
+  PetscReal, pointer :: rain_data(:)
 
   public :: rdycore_init
   public :: rdycore_run
@@ -31,6 +38,8 @@ contains
     !
     ! !LOCAL VARIABLES:
     character(len=1024)   :: config_file
+    PetscViewer           :: viewer
+    PetscInt              :: size
     PetscErrorCode        :: ierr
  
     config_file = 'rdycore.yaml'
@@ -46,6 +55,25 @@ contains
     PetscCallA(RDyCreate(PETSC_COMM_WORLD, config_file, rdy_, ierr))
     PetscCallA(RDySetup(rdy_, ierr))
  
+    ! allocate memory for grid-level rain data
+    PetscCallA(RDyGetNumLocalCells(rdy_, num_cells_owned, ierr))
+    allocate(rain_data(num_cells_owned))
+
+    ! Read the rain vector that has the following format:
+    !
+    !  time_1 rain_value_1
+    !  time_2 rain_value_2
+    !
+    PetscCallA(VecCreate(PETSC_COMM_WORLD, rain_timeseries, ierr))
+    PetscCallA(PetscViewerBinaryOpen(PETSC_COMM_WORLD, 'rain.bin', FILE_MODE_READ, viewer, ierr))
+    PetscCallA(VecLoad(rain_timeseries, viewer, ierr))
+    PetscCallA(PetscViewerDestroy(viewer, ierr))
+
+    ! Determine the number of rain
+    rain_stride = 2
+    PetscCallA(VecGetSize(rain_timeseries, size, ierr))
+    rain_nstep = size/rain_stride
+
   end subroutine rdycore_init
 
   !-----------------------------------------------------------------------
@@ -55,15 +83,53 @@ contains
     ! Initialize RDycore
     !
     ! !USES:
-    use RtmTimeManager, only : get_step_size
+    use RtmTimeManager, only : get_step_size, get_nstep, get_curr_time_string
     !
     implicit none
     !
     ! !LOCAL VARIABLES:
-    real(r8)       :: dtime
-    PetscErrorCode :: ierr
+    character(len=256)   :: dateTimeString
+    real(r8)             :: dtime
+    PetscScalar, pointer :: rain_p(:)
+    PetscInt             :: t, nstep
+    PetscReal            :: time_dn, time_up, cur_time, cur_rain
+    PetscBool            :: found
+    PetscErrorCode       :: ierr
 
-    dtime = get_step_size()
+    dtime    = get_step_size()
+    nstep    = get_nstep()
+    cur_time = (nstep-1)*dtime
+
+    call get_curr_time_string(dateTimeString)
+    if (masterproc) then
+       write(*,*)'Beginning timestep of RDycore  : ',trim(dateTimeString)
+       call shr_sys_flush(iulog)
+    end if
+
+
+    ! Find the current rainfall
+    PetscCallA(VecGetArrayF90(rain_timeseries, rain_p, ierr))
+    found = PETSC_FALSE
+
+    do t = 1, rain_nstep-1
+       time_dn = rain_p((t-1)*rain_stride + 1)
+       time_up = rain_p((t-1)*rain_stride + 3)
+       if (cur_time >= time_dn .and. cur_time < time_up) then
+          found = PETSC_TRUE
+          cur_rain = rain_p((t-1)*rain_stride + 2)
+          exit
+       end if
+    end do
+
+    if (.not.found) then
+       cur_rain = rain_p((nstep-1)*rain_stride + 2)
+    end if
+
+    PetscCallA(VecRestoreArrayF90(rain_timeseries, rain_p, ierr))
+
+    ! Set spatially homogeneous rainfall for all grid cells
+    rain_data(:) = cur_rain
+    PetscCallA(RDySetWaterSource(rdy_, rain_data, ierr))
 
     ! Set the coupling time step
     PetscCallA(RDySetCouplingInterval(rdy_, dtime, ierr))
@@ -86,8 +152,14 @@ contains
     ! !LOCAL VARIABLES:
     PetscErrorCode :: ierr
 
+    ! deallocate memory for rain data
+    deallocate(rain_data)
+    PetscCallA(VecDestroy(rain_timeseries, ierr))
+
+    ! destroy RDy object
     PetscCallA(RDyDestroy(rdy_, ierr));
- 
+
+    ! finalize
     PetscCallA(RDyFinalize(ierr));
  
   end subroutine rdycore_final
